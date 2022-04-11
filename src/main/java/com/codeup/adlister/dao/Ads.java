88ca997b34d51dfb494a5325d08d78df73daec3e
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-11 13:34:18 -0500

    mario

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -1,12 +1,13 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Deck;
 
 import java.util.List;
 
 public interface Ads {
     // get a list of all the ads
-    List<Ad> all();
+    List<Deck> all();
     // insert a new ad and return the new ad's id
-    Long insert(Ad ad);
+    Long insert(Deck deck);
 }
