1495255b403a7fbfce289c3d2d98e997cf904a44
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-11 10:15:50 -0500

    finished seeder file

--- migration.sql
+++ migration.sql
@@ -2,38 +2,43 @@ CREATE DATABASE MTGLister_db;
 
 USE MTGLister_db;
 
-# DROP TABLE IF EXISTS ads;
-# DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS deck;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS card;
 
-CREATE TABLE Users (
+CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    deck_list INT UNSIGNED NULL,
     FOREIGN KEY (deck_list) REFERENCES Deck(id)
         ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE Deck (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE deck (
+    deck_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     deck_name VARCHAR(50) not null,
     user_id INT UNSIGNED NOT NULL,
     date_created DATETIME NOT NULL,
     description TEXT NOT NULL,
     card_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (deck_id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
         ON DELETE CASCADE,
-    FOREIGN KEY (card_id ) REFERENCES Card(id)
-        ON DELETE CASCADE
-
 );
-CREATE TABLE Card (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE card (
+    card_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     card_name  VARCHAR(240) NOT NULL,
+    cmc INT UNSIGNED NOT NULL,
+    color_identity VARCHAR(45),
+    card_type VARCHAR(45),
+    rarity VARCHAR(45),
     text TEXT NOT NULL,
-    PRIMARY KEY (id)
-#                   need more
+    power INT NOT NULL,
+    toughness INT NOT NULL,
+    loyalty INT NOT NULL,
+    page INT NOT NULL,
+    PRIMARY KEY (card_id),
+    FOREIGN KEY (card_id) REFERENCES Deck(deck_id)
 );
