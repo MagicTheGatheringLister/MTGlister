771f4931581b2ad6ed2469a58c062f97002d70c5
Noah Bruno <noah.bruno122@gmail.com>
2022-04-11 17:06:19 -0500

    added search function, div for searched card, id of button and input

--- src/main/webapp/WEB-INF/fetchCards.js
+++ src/main/webapp/WEB-INF/fetchCards.js
@@ -1,30 +1,60 @@
-
-    let promises = [];
-    const fetchMagicCards = () => {
+let promises = [];
+const fetchMagicCards = () => {
 
     let url = `https://api.magicthegathering.io/v1/cards`;
     console.log(url)
     fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-    promises.push(json)
-    for(let i = 0; i < promises[0].cards.length; i++) {
-    console.log(promises[0].cards[i].imageUrl)
-    if (promises[0].cards[i].imageUrl === undefined){
-    console.log("none")
-}else{
-        $("#magic").append(`
-<div class="card-group">
-  <div class="card" style="width: 8rem;">
-    <img src="${promises[0].cards[i].imageUrl}" style="width: 220px; height: 300px;" class="card-img-top" alt="...">
-    <div class="card-body">
-    <button type="button" class="btn btn-outline-primary">Add to Deck</button>
+        .then((response) => response.json())
+        .then((json) => {
+            promises.push(json)
+            window.localStorage.setItem("promises", JSON.stringify(promises));
+            for(let i = 0; i < promises[0].cards.length; i++) {
+                console.log(promises[0].cards[i].imageUrl)
+                if (promises[0].cards[i].imageUrl == undefined){
+
+                }else{
+                    $("#magic").append(`
+<div class="card-group ">
+  <div class="card " style="width: 8rem;">
+    <img src="${promises[0].cards[i].imageUrl}" style="width: 235px; height: 300px;" class="card-img-top" alt="...">
+    <div class="card-body d-flex justify-content-center">
+    <button type="button" id="submit" class="btn btn-outline-primary">Add to Deck</button>
     </div>
     </div>
     </div>
        `)
+                }
+            }})};
+
+if (JSON.parse(window.localStorage.getItem('promises')) !== null) {
+    promises = JSON.parse(window.localStorage.getItem('promises'));
+    console.log("from local")
+    console.log(promises)
+} else {
+    console.log("new Request")
+    fetchMagicCards();
+    console.log(promises)
 }
 
-}})};
-
-    fetchMagicCards();
+let searchArray= [];
+$("#search").on("click", e => {
+    e.preventDefault();
+    let cardName = $("#input").val();
+    for (let i = 0; i < promises[0].cards.length; i++){
+        if(promises[0].cards[i].name.includes(cardName)){
+            searchArray = promises[0].cards[i].imageUrl;
+            console.log(searchArray)
+            $("#magic").empty();
+            $("#searchedCard").append(`
+<div class="card-group ">
+  <div class="card " style="width: 8rem;">
+    <img src="${searchArray}" style="width: 235px; height: 300px;" class="card-img-top" alt="...">
+    <div class="card-body d-flex justify-content-center">
+    <button type="button" id="submit" class="btn btn-outline-primary">Add to Deck</button>
+    </div>
+    </div>
+    </div>
+       `)
+        }
+    }
+})
