51a2a50c98eae7deeb1e1d667a964f40717d1f91
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-12 09:34:59 -0500

    added card servlet

---
+++ src/main/java/com/codeup/adlister/controllers/ChooseCardsServlet.java
@@ -0,0 +1,39 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Deck;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "controllers.ChooseCardsServlet", urlPatterns = "/ads/add-cards")
+public class ChooseCardsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        Deck deck = new Deck(
+                user.getId(),
+                request.getParameter("deck_name"),
+                request.getParameter("description"),
+                request.getParameter("date")
+        );
+
+        DaoFactory.getAdsDao().insert(deck);
+        response.sendRedirect("/ads");
+    }
+}
